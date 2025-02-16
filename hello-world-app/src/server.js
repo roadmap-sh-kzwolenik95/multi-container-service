6016ac126a719b433bce2a07c6c02f113bf69bf7
Krzysztof Zwolenik <kzwolenik95@gmail.com>
2025-02-16 21:17:30 +0100

    Develop the API

--- hello-world-app/src/server.js
+++ hello-world-app/src/server.js
@@ -1,23 +1,121 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 80;
 
-const auth = basicAuth({
-  users: {
-    [process.env.USERNAME]: process.env.PASSWORD,
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
   },
-  challenge: true,
-});
+  { timestamps: true }
+);
+
+const Todo = new mongoose.model("Todo", todoSchema);
+
+mongoose
+  .connect("mongodb://localhost:27017/todos", {
+    serverSelectionTimeoutMS: 5000,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => {
+    console.error("Could not connect to MongoDB", error);
+    process.exit(1);
+  });
+
+app.use(express.json());
 
 app.get("/todos", (req, res) => {
-  res.send("Todos!");
+  Todo.find()
+    .then((doc) => {
+      console.log("Listed:", doc);
+      res.status(200).json(doc);
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+      res.status(500).json({ error: "Failed to list todos" });
+    });
 });
 
 app.post("/todos", (req, res) => {
   const postData = req.body;
   console.log("Received POST data:", postData);
-  res.send("Data received successfully");
+
+  Todo.create(postData)
+    .then((doc) => {
+      console.log("Created:", doc);
+      res.status(201).json(doc);
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+      res.status(500).json({ error: "Failed to create todo" });
+    });
+});
+
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  Todo.findById(id)
+    .then((doc) => {
+      if (!doc) {
+        res.status(404).json({ error: "Todo not found" });
+      } else {
+        console.log("Todo:", doc);
+        res.status(200).json(doc);
+      }
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+      res.status(500).json({ error: "Failed to get todo" });
+    });
+});
+
+app.put("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  const putData = req.body;
+
+  Todo.findByIdAndUpdate(id, putData, {
+    new: true,
+    runValidators: true,
+  })
+    .then((doc) => {
+      if (!doc) {
+        res.status(404).json({ error: "Todo not found" });
+      } else {
+        console.log("Updated:", doc);
+        res.status(200).json(doc);
+      }
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+      res.status(500).json({ error: "Failed to update todo" });
+    });
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  Todo.findByIdAndDelete(id)
+    .then((doc) => {
+      if (!doc) {
+        res.status(404).json({ error: "Todo not found" });
+      } else {
+        console.log("Deleted:", doc);
+        res.status(204).end();
+      }
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+      res.status(500).json({ error: "Failed to delete todo" });
+    });
 });
 
 app.listen(port, "0.0.0.0", () => {
